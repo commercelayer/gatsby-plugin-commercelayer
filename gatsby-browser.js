b41492f7fed8609f489a350fcaaea7f06cadb211
Filippo Conforti <filippo@commercelayer.io>
2019-09-27 10:15:13 +0200

    Version bump 1.2.7 (updated lib to 1.9.16)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,33 +1,33 @@
 const scriptSource =
-  'https://cdn.jsdelivr.net/npm/commercelayer@1.9.14/dist/commercelayer.min.js'
+  "https://cdn.jsdelivr.net/npm/commercelayer@1.9.16/dist/commercelayer.min.js";
 
 const injectCommerceLayerScript = () => {
-  const script = document.createElement('script')
-  script.type = 'text/javascript'
-  script.src = scriptSource
-  document.body.appendChild(script)
-}
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = scriptSource;
+  document.body.appendChild(script);
+};
 
 const checkCommerceLayerTag = () => {
-  let scripts = document.getElementsByTagName('script')
+  let scripts = document.getElementsByTagName("script");
   for (let i = 0; i < scripts.length; i++) {
-    if (scripts[i].src === scriptSource) return true
+    if (scripts[i].src === scriptSource) return true;
   }
-  return false
-}
+  return false;
+};
 
 const initOrInjectCommerceLayer = () => {
   if (window.commercelayer && typeof window.commercelayer.init === `function`) {
-    window.commercelayer.init()
+    window.commercelayer.init();
   } else if (!checkCommerceLayerTag()) {
-    injectCommerceLayerScript()
+    injectCommerceLayerScript();
   }
-}
+};
 
 exports.onInitialClientRender = () => {
-  initOrInjectCommerceLayer()
-}
+  initOrInjectCommerceLayer();
+};
 
 exports.onRouteUpdate = () => {
-  initOrInjectCommerceLayer()
-}
+  initOrInjectCommerceLayer();
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,18 +1,18 @@
-'use strict'
+"use strict";
 
-var React = require('react')
+var React = require("react");
 
 exports.onRenderBody = function(_ref, pluginOptions) {
-  var setPostBodyComponents = _ref.setPostBodyComponents
+  var setPostBodyComponents = _ref.setPostBodyComponents;
 
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     var components = [
-      React.createElement('script', {
-        type: 'text/javascript',
+      React.createElement("script", {
+        type: "text/javascript",
         src:
-          'https://cdn.jsdelivr.net/npm/commercelayer@1.9.14/dist/commercelayer.min.js'
+          "https://cdn.jsdelivr.net/npm/commercelayer@1.9.16/dist/commercelayer.min.js"
       })
-    ]
-    return setPostBodyComponents(components)
+    ];
+    return setPostBodyComponents(components);
   }
-}
+};
